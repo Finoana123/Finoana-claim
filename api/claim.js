ac895ff95e8b38aa2ce4208e5a2d297e4b2960ca
Finoana123 <finoanarivoniaina@gmail.com>
2026-03-26 07:14:44 +0300

    Update claim.js

--- api/claim.js
+++ api/claim.js
@@ -1,1 +1,21 @@
 
+export default async function handler(req, res) {
+  try {
+    const TOKEN = process.env.TOKEN;
+
+    if (!TOKEN) {
+      return res.status(500).json({ error: "Token manquant" });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Mini bot prêt 🔥",
+      token_preview: TOKEN.substring(0, 10) + "..."
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message
+    });
+  }
+}
